b826cfcdb80b0b63bcf36517428a39ba21c82004
Vitali_Novik <vitaliy.novik.94@gmail.com>
2019-01-22 00:01:02 +0300

    temp

---
+++ JSCorePart1/src/js/filters/Configuration.js
@@ -0,0 +1,324 @@
+export const Filters = [
+    {
+        key: "language",
+        label: "Language",
+        values: [
+            {
+                key: "ar",
+                value: "Arabic"
+            },
+            {
+                key: "de",
+                value: "Deutsch"
+            },
+            {
+                key: "en",
+                value: "English"
+            },
+            {
+                key: "es",
+                value: "Español"
+            },
+            {
+                key: "fr",
+                value: "French"
+            },
+            {
+                key: "he",
+                value: "Hebrew"
+            },
+            {
+                key: "it",
+                value: "Italian"
+            },
+            {
+                key: "nl",
+                value: "Dutch"
+            },
+            {
+                key: "no",
+                value: "Norwegian"
+            },
+            {
+                key: "pt",
+                value: "Portuguese"
+            },
+            {
+                key: "ru",
+                value: "Русский"
+            },
+            {
+                key: "se",
+                value: "Northern Sami"
+            },
+            {
+                key: "ud",
+                value: "English (upside down)"
+            },
+            {
+                key: "zh",
+                value: "Chinese"
+            }
+        ]
+    },
+    {
+        key: "country",
+        label: "Countries",
+        values: [
+            {
+                key: "ae",
+                value: "UAE"
+            },
+            {
+                key: "ar",
+                value: "Argentina"
+            },
+            {
+                key: "at",
+                value: "Austria"
+            },
+            {
+                key: "au",
+                value: "Australia"
+            },
+            {
+                key: "be",
+                value: "Belgium"
+            },
+            {
+                key: "bg",
+                value: "Bulgaria"
+            },
+            {
+                key: "br",
+                value: "Brazil"
+            },
+            {
+                key: "ca",
+                value: "Canada"
+            },
+            {
+                key: "ch",
+                value: "Switzerland"
+            },
+            {
+                key: "cn",
+                value: "China"
+            },
+            {
+                key: "co",
+                value: "Colombia"
+            },
+            {
+                key: "cu",
+                value: "Cuba"
+            },
+            {
+                key: "cz",
+                value: "Czechia"
+            },
+            {
+                key: "de",
+                value: "Germany"
+            },
+            {
+                key: "eg",
+                value: "Egypt"
+            },
+            {
+                key: "fr",
+                value: "France"
+            },
+            {
+                key: "gb",
+                value: "United Kingdom"
+            },
+            {
+                key: "gr",
+                value: "Greece"
+            },
+            {
+                key: "hk",
+                value: "Hong Kong"
+            },
+            {
+                key: "hu",
+                value: "Hungary"
+            },
+            {
+                key: "id",
+                value: "Indonesia"
+            },
+            {
+                key: "ie",
+                value: "Ireland"
+            },
+            {
+                key: "il",
+                value: "Israel"
+            },
+            {
+                key: "in",
+                value: "India"
+            },
+            {
+                key: "it",
+                value: "Italy"
+            },
+            {
+                key: "jp",
+                value: "Japan"
+            },
+            {
+                key: "kr",
+                value: "Republic of Korea"
+            },
+            {
+                key: "lt",
+                value: "Lithuania"
+            },
+            {
+                key: "lv",
+                value: "Latvia"
+            },
+            {
+                key: "ma",
+                value: "Morocco"
+            },
+            {
+                key: "mx",
+                value: "Mexico"
+            },
+            {
+                key: "mx",
+                value: "Mexico"
+            },
+            {
+                key: "my",
+                value: "Malaysia"
+            },
+            {
+                key: "ng",
+                value: "Nigeria"
+            },
+            {
+                key: "nl",
+                value: "Netherlands"
+            },
+            {
+                key: "no",
+                value: "Norway"
+            },
+            {
+                key: "nz",
+                value: "New Zealand"
+            },
+            {
+                key: "ph",
+                value: "Philippines"
+            },
+            {
+                key: "pl",
+                value: "Poland"
+            },
+            {
+                key: "pt",
+                value: "Portugal"
+            },
+            {
+                key: "ro",
+                value: "Romania"
+            },
+            {
+                key: "rs",
+                value: "Serbia"
+            },
+            {
+                key: "ru",
+                value: "Russian Federation"
+            },
+            {
+                key: "sa",
+                value: "Saudi Arabia"
+            },
+            {
+                key: "se",
+                value: "Sweden"
+            },
+            {
+                key: "sg",
+                value: "Singapore"
+            },
+            {
+                key: "si",
+                value: "Slovenia"
+            },
+            {
+                key: "sk",
+                value: "Slovakia"
+            },
+            {
+                key: "th",
+                value: "Thailand"
+            },
+            {
+                key: "tr",
+                value: "Turkey"
+            },
+            {
+                key: "tw",
+                value: "Taiwan"
+            },
+            {
+                key: "ua",
+                value: "Ukraine"
+            },
+            {
+                key: "us",
+                value: "USA"
+            },
+            {
+                key: "ve",
+                value: "Venezuela"
+            },
+            {
+                key: "za",
+                value: "Zambia"
+            }
+        ]
+    },
+    {
+        key: "category",
+        label: "Category",
+        values: [
+            {
+                key: "business",
+                value: "Business"
+            },
+            {
+                key: "entertainment",
+                value: "Entertainment"
+            },
+            {
+                key: "general",
+                value: "General"
+            },
+            {
+                key: "health",
+                value: "Health"
+            },
+            {
+                key: "science",
+                value: "Science"
+            },
+            {
+                key: "sports",
+                value: "Sports"
+            },
+            {
+                key: "technology",
+                value: "Technology"
+            }
+        ]
+    }
+];
